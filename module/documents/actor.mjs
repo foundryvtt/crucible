b61d2aa240f1b047abf8381427215342c477a0ca
danielrab.work <s17b1_rabinovich@179.ru>
2024-09-27 20:56:44 +0300

    make the warning and page switching work on levels other than zero

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -1673,23 +1673,21 @@ export default class CrucibleActor extends Actor {
     if ( delta === 0 ) return;
 
     // Confirm that character creation is complete
-    if ( this.isL0 ) {
-      if (!this.system.details.ancestry?.name) {
-        game.packs.get(SYSTEM.COMPENDIUM_PACKS.ancestry).render(true)
-        return ui.notifications.warn("WALKTHROUGH.AddAncestry", {localize: true});
-      } else if (!this.system.details.background?.name) {
-        game.packs.get(SYSTEM.COMPENDIUM_PACKS.background).render(true)
-        return ui.notifications.warn("WALKTHROUGH.AddBackground", {localize: true});
-      } else if (this.points.ability.requireInput) {
-        this.sheet.changeTab("attributes", "sheet");
-        return ui.notifications.warn("WALKTHROUGH.AbilityPoints", {localize: true});
-      } else if (this.points.skill.available) {
-        this.sheet.changeTab("skills", "sheet");
-        return ui.notifications.warn("WALKTHROUGH.SkillPoints", {localize: true});
-      } else if (this.points.talent.available) {
-        this.sheet.changeTab("talents", "sheet");
-        return ui.notifications.warn("WALKTHROUGH.TalentPoints", {localize: true});
-      }
+    if (!this.system.details.ancestry?.name) {
+      game.packs.get(SYSTEM.COMPENDIUM_PACKS.ancestry).render(true)
+      return ui.notifications.warn("WALKTHROUGH.AddAncestry", {localize: true});
+    } else if (!this.system.details.background?.name) {
+      game.packs.get(SYSTEM.COMPENDIUM_PACKS.background).render(true)
+      return ui.notifications.warn("WALKTHROUGH.AddBackground", {localize: true});
+    } else if (this.points.ability.requireInput) {
+      this.sheet.changeTab("attributes", "sheet");
+      return ui.notifications.warn("WALKTHROUGH.AbilityPoints", {localize: true});
+    } else if (this.points.skill.available) {
+      this.sheet.changeTab("skills", "sheet");
+      return ui.notifications.warn("WALKTHROUGH.SkillPoints", {localize: true});
+    } else if (this.points.talent.available) {
+      this.sheet.changeTab("talents", "sheet");
+      return ui.notifications.warn("WALKTHROUGH.TalentPoints", {localize: true});
     }
 
     // Commit the update
