f1150dcb6e742d5d8872cc95b61e7222f4ad0f33
Andrew (Atropos) <admin@foundryvtt.com>
2024-09-28 15:11:16 -0400

    Additional bug fixes for 0.6.1 release

--- module/applications/sheets/ancestry.mjs
+++ module/applications/sheets/ancestry.mjs
@@ -16,4 +16,21 @@ export default class AncestrySheet extends CrucibleBaseItemSheet {
   static {
     this._initializeItemSheetClass()
   }
+
+  /* -------------------------------------------- */
+
+  /** @override */
+  _processFormData(event, form, formData) {
+    const submitData = super._processFormData(event, form, formData);
+
+    // Only allow (primary,secondary) or (resistance,vulnerability) to be submitted if both are defined
+    const pairs = [["primary", "secondary"], ["resistance", "vulnerability"]];
+    for ( const [a, b] of pairs ) {
+      if ( !(submitData.system[a] && submitData.system[b]) ) {
+        delete submitData.system[a];
+        delete submitData.system[b];
+      }
+    }
+    return submitData;
+  }
 }
