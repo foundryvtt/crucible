f0e4cfe8d6fb0b4ddbc1088b9fe3c203c1241ff4
dev7355608 <eberhard.hoepfner@gmail.com>
2025-03-28 15:05:06 +0100

    Implement `CrucibleTokenRuler`

--- module/canvas/_module.mjs
+++ module/canvas/_module.mjs
@@ -1,4 +1,4 @@
 export * as tree from "./tree/_module.mjs";
 export * as grid from "./grid.mjs";
-export {default as CrucibleRuler} from "./ruler.mjs";
 export {default as CrucibleTokenObject} from "./token.mjs";
+export {default as CrucibleTokenRuler} from "./token-ruler.mjs";
