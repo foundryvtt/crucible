d1ec9a1acdc5619ad5c5634a6f41a05593593869
Andrew (Atropos) <admin@foundryvtt.com>
2025-05-29 21:57:54 -0400

    Add custom token measurement waypoint labels

--- module/canvas/token-ruler.mjs
+++ module/canvas/token-ruler.mjs
@@ -2,56 +2,23 @@
 export default class CrucibleTokenRuler extends foundry.canvas.placeables.tokens.TokenRuler {
 
   /** @override */
-  static WAYPOINT_TEMPLATE = "systems/crucible/templates/hud/token-ruler-waypoint-label.hbs";
+  static WAYPOINT_LABEL_TEMPLATE = "systems/crucible/templates/hud/token-ruler-waypoint-label.hbs";
+
+  /* -------------------------------------------- */
 
   /** @override */
-  _prepareWaypointData(waypoints) {
-    const waypointData = [];
-    let hasElevation = false;
-    for ( const waypoint of waypoints ) {
-      const {index, elevation, explicit, next, previous, ray, teleport} = waypoint;
-      if ( !previous || (!explicit && next && !teleport) ) continue;
+  _getWaypointLabelContext(waypoint, state) {
+    const context = super._getWaypointLabelContext(waypoint, state);
+    if ( !context || !this.token.actor ) return context;
+    const grid = canvas.scene?.grid;
+    if ( !grid || (grid.distance !== 1) || (grid.units !== "ft") ) return;
 
-      // Prepare data structure
-      const data = {
-        action: CONFIG.Token.movement.actions[waypoint.action],
-        cssClass: [
-          waypoint.hidden ? "secret" : "",
-          waypoint.next ? "" : "last"
-        ].filterJoin(" "),
-        secret: waypoint.hidden,
-        units: canvas.grid.units,
-        costUnits: "AP",
-        uiScale: canvas.dimensions.uiScale,
-        waypoint
-      };
-
-      // Label Position
-      const deltaElevation = elevation - previous.elevation;
-      if ( (ray.distance === 0) && (deltaElevation === 0) ) continue;
-      data.position = {x: ray.B.x, y: ray.B.y + (next ? 0 : 0.5 * this.token.h) + (16 * canvas.dimensions.uiScale)};
-
-      // Segment Distance
-      data.totalDistance = waypoint.measurement.distance.toNearest(0.01).toLocaleString(game.i18n.lang);
-      if ( index >= 2 ) data.deltaDistance =  waypoint.measurement.backward.distance.toNearest(0.01).signedString();
-
-      // Segment Cost
-      const stride = this.token.actor?.system.movement.stride ?? 10;
-      const cost = waypoint.measurement.cost / stride;
-      const deltaCost = waypoint.cost / stride;
-      data.totalCost = Number.isFinite(cost) ? cost.toNearest(0.001).toLocaleString(game.i18n.lang) : "∞";
-      if ( index >= 2 ) data.deltaCost = Number.isFinite(deltaCost) ? deltaCost.toNearest(0.001).signedString() : "∞";
-
-      // Elevation
-      if ( elevation !== 0 ) hasElevation = true;
-      data.hasElevation = hasElevation;
-      if ( data.hasElevation ) {
-        data.elevationIcon = "fa-arrows-up-down";
-        data.totalElevation = elevation;
-        if ( deltaElevation !== 0 ) data.deltaElevation = deltaElevation.signedString();
-      }
-      waypointData.push(data);
-    }
-    return waypointData;
+    // Segment Cost
+    // TODO needs to incorporate free movement or other movement cost modifiers
+    const stride = this.token.actor?.system.movement.stride ?? 8;
+    const totalCost = Math.ceil(waypoint.measurement.cost / stride);
+    context.distance.units = grid.units;
+    context.cost = {total: totalCost, units: "A"};
+    return context;
   }
 }
