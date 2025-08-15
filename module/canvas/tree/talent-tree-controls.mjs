fc12de660f22a2dbf1d59334656489c94f7c5a3f
Jan <janweigmann@web.de>
2025-08-15 08:22:57 +0200

    fixes #276

--- module/canvas/tree/talent-tree-controls.mjs
+++ module/canvas/tree/talent-tree-controls.mjs
@@ -1,16 +1,30 @@
+const {ApplicationV2, HandlebarsApplicationMixin} = foundry.applications.api;
 /**
  * An Application instance that renders the talent tree controls UI element.
  */
-export default class CrucibleTalentTreeControls extends Application {
+export default class CrucibleTalentTreeControls extends HandlebarsApplicationMixin(ApplicationV2) {
 
   /** @inheritdoc */
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      id: "crucible-talent-controls",
-      template: "systems/crucible/templates/hud/talent-tree-controls.hbs",
-      popOut: false
-    });
-  }
+  static DEFAULT_OPTIONS = {
+    id: "crucible-talent-controls",
+    window: {
+      frame: false,
+      positioned: false,
+    },
+    classes: ['crucible', 'flexrow'],
+    actions: {
+      reset: this._onReset,
+      closeTree: this._onCloseTree
+    }
+  };
+
+  /** @override */
+  static PARTS = {
+    controls: {
+      root: true,
+      template: 'systems/crucible/templates/hud/talent-tree-controls.hbs',
+    },
+  };
 
   /* -------------------------------------------- */
 
@@ -25,7 +39,7 @@ export default class CrucibleTalentTreeControls extends Application {
   /* -------------------------------------------- */
 
   /** @override */
-  getData(options = {}) {
+  async _prepareContext(options) {
     return {
       actor: this.tree.actor,
       cssClass: this.options.classes.join(" ")
@@ -34,23 +48,28 @@ export default class CrucibleTalentTreeControls extends Application {
 
   /* -------------------------------------------- */
 
-  _replaceHTML(element, html) {
-    super._replaceHTML(element, html);
-    crucible.tree.canvas.insertAdjacentElement("afterend", this._element[0]);
-  }
-
-  /* -------------------------------------------- */
-
-  _injectHTML(html) {
-    this._element = html;
-    crucible.tree.canvas.insertAdjacentElement("afterend", this._element[0]);
-  }
-
-  /* -------------------------------------------- */
-
   /** @override */
-  activateListeners(html) {
-    html.find("button.reset").click(() => this.tree.actor.resetTalents({dialog: true}));
-    html.find("button.close").click(() => this.tree.close());
+  _insertElement(element) {
+    const existing = document.getElementById(element.id);
+    if ( existing ) existing.replaceWith(element);
+    else crucible.tree.canvas.insertAdjacentElement("afterend", element);
+  }
+  
+  /* -------------------------------------------- */
+
+  /**
+   * @this {CrucibleTalentTreeControls}
+   * @type {ApplicationClickAction}
+   */
+  static _onCloseTree() {
+    this.tree.close();
+  }
+
+  /**
+   * @this {CrucibleTalentTreeControls}
+   * @type {ApplicationClickAction}
+   */
+  static _onReset() {
+    this.tree.actor.resetTalents({ dialog: true });
   }
 }
