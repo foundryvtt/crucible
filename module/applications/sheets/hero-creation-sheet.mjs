8c729cc6c779bd6bc17c31d0dd5668816f7e372c
Andrew (Atropos) <admin@foundryvtt.com>
2025-10-21 21:53:44 -0400

    Refine character creation process with support for Ember requirements

--- module/applications/sheets/hero-creation-sheet.mjs
+++ module/applications/sheets/hero-creation-sheet.mjs
@@ -131,6 +131,12 @@ export default class CrucibleHeroCreationSheet extends HandlebarsApplicationMixi
     return obj;
   }, {}));
 
+  /**
+   * Record when the entire process is complete so we can avoid displaying warnings on close.
+   * @type {boolean}
+   */
+  #complete = false;
+
   /* -------------------------------------------- */
 
   get steps() {
@@ -179,6 +185,7 @@ export default class CrucibleHeroCreationSheet extends HandlebarsApplicationMixi
    */
   async _initializeState() {
     this._state.name = this._clone.name;
+    this.#complete = false;
     const promises = [];
     for ( const step of Object.values(this.constructor.STEPS) ) {
       if ( step?.initialize instanceof Function ) promises.push(step.initialize.call(this));
@@ -564,7 +571,7 @@ export default class CrucibleHeroCreationSheet extends HandlebarsApplicationMixi
   /** @override */
   _canRender(_options) {
     super._canRender(_options);
-    if ( (this.document.type !== "hero") || (this.document.level > 0) ) {
+    if ( !this.#complete && ((this.document.type !== "hero") || (this.document.level > 0)) ) {
       throw new Error("You may only use the CrucibleHeroCreationSheet for a hero Actor which is level zero.")
     }
   }
@@ -596,7 +603,7 @@ export default class CrucibleHeroCreationSheet extends HandlebarsApplicationMixi
 
   /** @inheritDoc */
   async close(options={}) {
-    if ( options.dialog !== false ) {
+    if ( (options.dialog !== false) && !this.#complete ) {
       crucible.api.audio.playClick();
       const confirm = await foundry.applications.api.DialogV2.confirm({
         window: {
@@ -818,12 +825,13 @@ export default class CrucibleHeroCreationSheet extends HandlebarsApplicationMixi
   static async #onComplete() {
     this._state.name = this.element.querySelector("#hero-creation-name").value.trim();
     const creationData = this._clone.toObject();
-    const creationOptions = {recursive: false, diff: false, noHook: true};
+    const creationOptions = {recursive: false, diff: false, noHook: true, render: false};
     await this._finalizeCreationData(creationData, creationOptions);
 
     // Update the actor and render the regular sheet
+    this.#complete = true;
     await this.document.update(creationData, creationOptions);
-    this.document.sheet.render({force: true});
+    await this.close({dialog: false});
   }
 
   /* -------------------------------------------- */
