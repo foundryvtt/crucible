b5a77bd64c61944e1b981fef496513fc6309be14
Michael Roth <michaelhroth624@gmail.com>
2025-11-13 16:50:52 -0500

    show (maybe) targeted rune/gesture in counterspell dialog

--- module/dice/counterspell-dialog.mjs
+++ module/dice/counterspell-dialog.mjs
@@ -8,4 +8,18 @@ export default class CounterspellDialog extends SpellCastDialog {
   /** @override */
   static TEMPLATE = "systems/crucible/templates/dice/counterspell-dialog.hbs";
 
+  /** @inheritDoc */
+  async _prepareContext(options) {
+    const context = await super._prepareContext(options);
+    const actor = this.action.actor;
+
+    // Targeted spell
+    // TODO: War Mage?
+    const {rune, gesture} = ChatMessage.implementation.getLastAction();
+    context.targetedRune = (actor.talentIds.has("recognizespellcr") && actor.grimoire.runes.has(rune)) ? rune.name : "Unknown";
+    context.targetedGesture = "Unknown";
+    
+    return context;
+  }
+
 }
