0546ce4aef6769e7ae9a77f53eb06712cbc975aa
Michael Roth <michaelhroth624@gmail.com>
2026-04-12 17:04:50 -0400

    ensure targets are displayed regardless of number of rolls

--- module/models/action.mjs
+++ module/models/action.mjs
@@ -2467,7 +2467,7 @@ export default class CrucibleAction extends foundry.abstract.DataModel {
       if ( !event.roll ) continue;
       const isNewTarget = !targetActors.has(event.target);
       if ( isNewTarget ) targetActors.add(event.target);
-      event.roll.data.newTarget = isNewTarget && (targetActors.size > 1);
+      event.roll.data.newTarget = isNewTarget && (this.eventsByTarget.size > 1);
       event.roll.data.index = rolls.length;
       rolls.push(event.roll);
     }
@@ -2494,6 +2494,7 @@ export default class CrucibleAction extends foundry.abstract.DataModel {
       context: this.usage.context,
       hasActionTags: !tags.action.empty,
       hasContextTags: !tags.context.empty,
+      hasTargetTags: (targets.length === 1) || (targets.length && !this.eventsByTarget.values().some(e => e.roll)),
       hasTargets: !["self", "none"].includes(this.target.type),
       tags,
       targets,
