9062f6bbda6dacdef86a9db7d36f84566c900038
Michael Roth <michaelhroth624@gmail.com>
2026-04-13 12:04:55 -0400

    clean up `talisman` tag's `canUse`, remove `ranged` tag from refocus

--- module/const/action.mjs
+++ module/const/action.mjs
@@ -312,9 +312,9 @@ export const TAGS = {
     canUse() {
       const {mainhand: mh, offhand: oh} = this.actor.equipment.weapons;
       const categories = ["talisman1", "talisman2"];
-      if ( !categories.includes(mh?.category) && !categories.includes(oh?.category) ) return false;
-      if ( !this.usage.strikes.every(w => w.config.category.training.includes("talisman")) ) {
-        throw new Error(_loc("ACTION.WARNINGS.RequiresTalisman"));
+      if ( (!categories.includes(mh?.category) && !categories.includes(oh?.category))
+        || !this.usage.strikes.every(w => w.config.category.training.includes("talisman")) ) {
+        throw new Error(_loc("ACTION.WARNINGS.RequiresTalisman", {action: this.name}));
       }
     }
   },
