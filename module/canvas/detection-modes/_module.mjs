70c601e689b0711933098be0ce9fb2ecbeaaf004
Michael Roth <michaelhroth624@gmail.com>
2026-05-13 14:55:59 -0400

    add echolocation detection mode, clean up constructor of others

--- module/canvas/detection-modes/_module.mjs
+++ module/canvas/detection-modes/_module.mjs
@@ -1,2 +1,3 @@
+export {default as DetectionModeEcholocation} from "./echolocation.mjs"
 export {default as DetectionModeSenseCreature} from "./sense-creature.mjs";
 export {default as DetectionModeThermalVision} from "./thermal-vision.mjs";
