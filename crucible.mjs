601f29c1597c5daa7f4ce356a2b94ae3a1b03c90
roth-michael <72220390+roth-michael@users.noreply.github.com>
2026-09-17 17:43:37 -0400

    Implement custom config app for "Crucible Action" region behavior (#1474)
    * implement custom config app for action behavior
    
    * replace `oncePerRound` with `frequency`
    
    * remove unnecessary whitespace
    
    * restyle config, address review feedback
    
    * restyle action behavior config to be closer to normal action config
    
    * remove currently unnecessary effect schema definition function

--- crucible.mjs
+++ crucible.mjs
@@ -255,6 +255,14 @@ Hooks.once("init", async function() {
 
   sheets.registerSheet(JournalEntry, "crucible", applications.CrucibleJournalSheet, {label: "CRUCIBLE.SHEETS.Journal"});
 
+  sheets.unregisterSheet(RegionBehavior, "core", foundry.applications.sheets.RegionBehaviorConfig, {
+    types: ["crucible.action"]
+  });
+  sheets.registerSheet(RegionBehavior, "crucible", applications.CrucibleActionBehaviorRegionConfig, {
+    types: ["crucible.action"],
+    label: "CRUCIBLE.SHEETS.ActionBehavior"
+  });
+
   // Core Application Overrides
   CONFIG.ui.combat = applications.CrucibleCombatTracker;
 
@@ -510,6 +518,11 @@ Hooks.once("i18nInit", function() {
   // Localize DataModels
   foundry.helpers.Localization.localizeDataModel(models.CrucibleAction);
   foundry.helpers.Localization.localizeSchema(models.CrucibleAction.schema.fields.effects.element, ["EFFECT"], {prefixPath: "effects.element."});
+
+  // TODO: Could this be cleaner?
+  const actionBehaviorEffectSchema = models.CrucibleActionRegionBehavior.schema.fields.action.fields.effects.element;
+  foundry.helpers.Localization.localizeSchema(actionBehaviorEffectSchema, ["EFFECT"], {prefixPath: "system.action.effects.element."});
+  foundry.helpers.Localization.localizeSchema(actionBehaviorEffectSchema, ["ACTION"], {prefixPath: "system.action."});
   foundry.helpers.Localization.localizeDataModel(models.CrucibleSpellAction);
   foundry.helpers.Localization.localizeDataModel(models.CrucibleCounterspellAction);
 
---
+++ module/applications/config/action-behavior-config.mjs
@@ -0,0 +1,195 @@
+/**
+ * The Region Behavior configuration application specific to Crucible Action behaviors.
+ */
+export default class CrucibleActionBehaviorRegionConfig extends foundry.applications.sheets.RegionBehaviorConfig {
+
+  /** @inheritDoc */
+  static DEFAULT_OPTIONS = {
+    actions: {
+      addEffect: CrucibleActionBehaviorRegionConfig.#onAddEffect,
+      deleteEffect: CrucibleActionBehaviorRegionConfig.#onDeleteEffect
+    },
+    classes: ["crucible", "action-behavior", "action", "standard-form"],
+    position: {width: 600, height: "auto"},
+    form: {
+      submitOnChange: true,
+      closeOnSubmit: false
+    },
+    sheetConfig: false
+  };
+
+  /**
+   * A template partial used for rendering an Active Effect inside this Region Behavior.
+   * @type {string}
+   */
+  static ACTIVE_EFFECT_PARTIAL = "systems/crucible/templates/sheets/action/effect.hbs";
+
+  /** @override */
+  static PARTS = {
+    header: {
+      id: "header",
+      template: "systems/crucible/templates/sheets/region-behavior/action-behavior-header.hbs"
+    },
+    tabs: {
+      id: "tabs",
+      template: "templates/generic/tab-navigation.hbs"
+    },
+    action: {
+      id: "action",
+      template: "systems/crucible/templates/sheets/region-behavior/action-behavior-action.hbs"
+    },
+    effects: {
+      id: "effects",
+      template: "systems/crucible/templates/sheets/region-behavior/action-behavior-effects.hbs",
+      templates: [CrucibleActionBehaviorRegionConfig.ACTIVE_EFFECT_PARTIAL],
+      scrollable: [""]
+    },
+    behavior: {
+      id: "behavior",
+      template: "systems/crucible/templates/sheets/region-behavior/action-behavior-behavior.hbs"
+    }
+  };
+
+  /** @override */
+  static TABS = {
+    sheet: {
+      tabs: [
+        {id: "action", icon: "fa-solid fa-book"},
+        {id: "effects", icon: "fa-solid fa-hourglass-clock"},
+        {id: "behavior", icon: "fa-solid fa-gear"}
+      ],
+      initial: "action",
+      labelPrefix: "REGION_BEHAVIORS.ACTION.TABS"
+    }
+  };
+
+  /** @override */
+  get title() {
+    return _loc("REGION_BEHAVIORS.ACTION.ConfigTitle", {action: this.document.name});
+  }
+
+  /* -------------------------------------------- */
+  /*  Rendering                                   */
+  /* -------------------------------------------- */
+
+  /** @inheritDoc */
+  async _prepareContext(options) {
+    const context = await super._prepareContext(options);
+
+    // Remove auto-added system fields, transform into more easily accessed structure
+    context.fields = context.fields.slice(0, -1).reduce((acc, {fields}) => {
+      for ( const {field, value} of fields ) {
+        acc[field.name] = {field, value};
+      }
+      return acc;
+    });
+
+    return {
+      ...context,
+      effectPartial: this.constructor.ACTIVE_EFFECT_PARTIAL,
+      effects: this.#prepareEffects(),
+      headerTags: this.document.system.action.tags.reduce((acc, tagId) => {
+        const tag = SYSTEM.ACTION.TAGS[tagId];
+        if ( !tag.internal ) acc[tagId] = tag;
+        return acc;
+      }, {}),
+      tabs: this._prepareTabs("sheet"),
+      tags: this.#prepareTags(),
+      targetScopes: SYSTEM.ACTION.TARGET_SCOPES.choices,
+      systemFields: this.document.system.schema.fields,
+      effectDurations: CONST.ACTIVE_EFFECT_DURATION_UNITS.reduce((acc, v) => {
+        if ( ["months", "turns"].includes(v) ) return acc;
+        return [...acc, {value: v, label: _loc(`EFFECT.DURATION.UNITS.${v}`)}];
+      }, []),
+      effectExpiryEvents: Object.entries(ActiveEffect.EXPIRY_EVENTS).map(([k, v]) => ({value: k, label: _loc(v)}))
+    };
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Prepare effects array for this behavior's Action.
+   * @returns {object[]}
+   */
+  #prepareEffects() {
+    const effects = this.document.system.toObject().action.effects;
+    for ( const [i, effect] of effects.entries() ) {
+      effect.fieldPath = `system.action.effects.${i}`;
+    }
+    return effects;
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Prepare tag options and selections for this behavior's Action.
+   * @returns {FormSelectOption[]}
+   */
+  #prepareTags() {
+    const tags = [];
+    for ( const t of Object.values(SYSTEM.ACTION.TAGS) ) {
+      if ( t.internal ) continue;
+      const cat = SYSTEM.ACTION.TAG_CATEGORIES[t.category];
+      const group = cat?.label;
+      const selected = this.document.system.action.tags.has(t.tag);
+      tags.push({value: t.tag, label: t.label, group, selected});
+    }
+    return tags;
+  }
+
+  /* -------------------------------------------- */
+  /*  Event Listeners and Handlers                */
+  /* -------------------------------------------- */
+
+  /**
+   * Add an effect to this behavior's Action.
+   * @this {CrucibleActionBehaviorRegionConfig}
+   * @param {PointerEvent} _event
+   * @param {HTMLElement} _target
+   * @returns {Promise<void>}
+   */
+  static async #onAddEffect(_event, _target) {
+    const effects = this.document.system.toObject().action.effects;
+    effects.push({
+      scope: SYSTEM.ACTION.TARGET_SCOPES.ENEMIES,
+      duration: {
+        value: 1,
+        units: "rounds",
+        expiry: "turnEnd"
+      }
+    });
+    this.document.updateSource({"system.action.effects": effects});
+    await this.render();
+    this.document.updateSource({"system.action.effects": effects.slice(0, -1)});
+    const submit = new SubmitEvent("submit", {cancelable: true});
+    this.element.dispatchEvent(submit);
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Delete an effect from this behavior's Action.
+   * @this {CrucibleActionBehaviorRegionConfig}
+   * @param {PointerEvent} _event
+   * @param {HTMLElement} target
+   * @returns {Promise<void>}
+   */
+  static async #onDeleteEffect(_event, target) {
+    const fieldset = target.closest("fieldset.effect");
+    fieldset.remove();
+    const submit = new SubmitEvent("submit", {cancelable: true});
+    this.element.dispatchEvent(submit);
+  }
+
+  /* -------------------------------------------- */
+  /*  Form Submission                             */
+  /* -------------------------------------------- */
+
+  /** @override */
+  _processFormData(event, form, formData) {
+    const data = foundry.utils.expandObject(formData.object);
+    data.system.action.effects = Object.values(data.system.action.effects || {});
+    data.name = data.system.action.name;
+    return data;
+  }
+}
